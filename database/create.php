98299b0657df5857b05a3f7b56d1e4090a29cf69
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2023-04-01 20:07:06 +0530

    add db file structure

---
+++ database/create.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Simple example of extending the SQLite3 class and changing the __construct
+ * parameters, then using the open method to initialize the DB.
+ */
+class MyDB extends SQLite3
+{
+    function __construct()
+    {
+        $this->open('URL_list.db');
+    }
+}
+
+$db = new MyDB();
+
+$db->exec('CREATE TABLE foo (bar STRING)');
+$db->exec("INSERT INTO foo (bar) VALUES ('This is a test')");
+
+$result = $db->query('SELECT bar FROM foo');
+var_dump($result->fetchArray());
+?>
